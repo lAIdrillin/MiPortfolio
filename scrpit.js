bc3b9e05ddfcccb4c3224ca3fab48de1ab29e253
lAIdrillin <gabrielgcastro1809@gmail.com>
2025-06-02 20:29:13 +0200

    i

--- scrpit.js
+++ scrpit.js
@@ -13,31 +13,31 @@ function verDetalle(proyecto){
         contenido = `
        <h2>Memorion</h2>
        <p>Menu de inicio</p>
-      <img src="images/juego_inicio.png" alt="Cartas" class="img-bloque">
+      <img src="imagenes/juego_inicio.png" alt="Cartas" class="img-bloque">
       <p>historial</p>
-      <img src="images/juego_historial.png" alt="Cartas" class="img-bloque">
+      <img src="imagenes/juego_historial.png" alt="Cartas" class="img-bloque">
       <p>Records</p>
-      <img src="images/juego_records.png" alt="Cartas" class="img-bloque">
+      <img src="imagenes/juego_records.png" alt="Cartas" class="img-bloque">
       <p>Tablero</p>
-      <img src="images/juego_tablero.png" alt="Cartas" class="img-bloque">
+      <img src="imagenes/juego_tablero.png" alt="Cartas" class="img-bloque">
     `;
     } else if (proyecto === 'calculadora'){
         contenido = `
         <h2>CalculadoraIp</h2>
          <p>Menu de inicio</p>
-        <img src="images/calculadora_inicio.png" alt="calculadora" class="img-bloque">
+        <img src="imagenes/calculadora_inicio.png" alt="calculadora" class="img-bloque">
         <p>Detalles de una ip</p>
-        <img src="images/calculadora.png" alt="calculadora" class="img-bloque">
+        <img src="imagenes/calculadora.png" alt="calculadora" class="img-bloque">
         `;
     } else if(proyecto === 'appmaps') {
         contenido = `
         <h2>AppMaps</h2>
         <p>Menu de inicio</p>
-        <img src="images/mapa1.png" alt="Mapa" class="img-bloque">
+        <img src="imagenes/mapa1.png" alt="Mapa" class="img-bloque">
         <p>Ejemplo mapa con marcadores en Madrid</p>
-        <img src="images/mapa2.png" alt="Mapa" class="img-bloque">
+        <img src="imagenes/mapa2.png" alt="Mapa" class="img-bloque">
         <p>Detector de terremotos</p>
-        <img src="images/terremoto.png" alt="Mapa" class="img-bloque">
+        <img src="imagenes/terremoto.png" alt="Mapa" class="img-bloque">
         `;
     }
 detalleDiv.innerHTML = contenido;
