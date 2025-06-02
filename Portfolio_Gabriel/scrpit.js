f35ba5c4ceb5509494ed52b877f05323b7ba514c
lAIdrillin <gabrielgcastro1809@gmail.com>
2025-06-02 19:40:17 +0200

    s

--- Portfolio_Gabriel/scrpit.js
+++ Portfolio_Gabriel/scrpit.js
@@ -12,29 +12,32 @@ function verDetalle(proyecto){
     if(proyecto === 'memorion') {
         contenido = `
        <h2>Memorion</h2>
+       <p>Menu de inicio</p>
       <img src="images/juego_inicio.png" alt="Cartas" class="img-bloque">
+      <p>historial</p>
       <img src="images/juego_historial.png" alt="Cartas" class="img-bloque">
+      <p>Records</p>
       <img src="images/juego_records.png" alt="Cartas" class="img-bloque">
+      <p>Tablero</p>
       <img src="images/juego_tablero.png" alt="Cartas" class="img-bloque">
     `;
     } else if (proyecto === 'calculadora'){
         contenido = `
         <h2>CalculadoraIp</h2>
-        <img src="images/calculadora.png" alt="calculadora" class="img-bloque">
-        <p>Detalles de una ip</p>
+         <p>Menu de inicio</p>
         <img src="images/calculadora_inicio.png" alt="calculadora" class="img-bloque">
-        <p>Menu de inicio</p>
+        <p>Detalles de una ip</p>
+        <img src="images/calculadora.png" alt="calculadora" class="img-bloque">
         `;
     } else if(proyecto === 'appmaps') {
         contenido = `
         <h2>AppMaps</h2>
-        <img src="images/mapa1.png" alt="Mapa" class="img-bloque">
         <p>Menu de inicio</p>
-        <img src="images/mapa2.png" alt="Mapa" class="img-bloque">
+        <img src="images/mapa1.png" alt="Mapa" class="img-bloque">
         <p>Ejemplo mapa con marcadores en Madrid</p>
-        <img src="images/terremoto.png" alt="Mapa" class="img-bloque">
+        <img src="images/mapa2.png" alt="Mapa" class="img-bloque">
         <p>Detector de terremotos</p>
-
+        <img src="images/terremoto.png" alt="Mapa" class="img-bloque">
         `;
     }
 detalleDiv.innerHTML = contenido;
